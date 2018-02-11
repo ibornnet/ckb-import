aec055ade2a6df949548b65bb2edc357c92d2d37
novica <nnovica@gmail.com>
2018-02-11 09:01:09 +0100

    python code

---
+++ ckb.py
@@ -0,0 +1,42 @@
+#Convert CKB bank statement
+import sys, codecs
+
+#open the file. read everything except line with totals
+BankStatement = codecs.open(sys.argv[1], 'r', 'cp1251')
+TransactionsList = BankStatement.readlines()[:-1]
+
+
+#open file for writing; append header line.
+ManagerStatement = codecs.open(sys.argv[1]+"mng.csv", 'a+', 'utf-8')
+ManagerStatement.write("Date, Description, Amount\n")
+
+#list to select just the needed elements
+SelectList = [1, 5 ,6, 9]
+
+for Transaction in TransactionsList:
+
+    DateDescriptionAmmoutList = []
+    TransactionElements=(Transaction.split("\t"))
+
+    #Remove elements from list. Keep only 1, 5, 6, and 9.
+    NewList = [TransactionElements[i] for i in SelectList]
+
+    DateDescriptionAmmoutList.append(NewList[0])
+    DateDescriptionAmmoutList.append(NewList[3].replace(","," "))
+
+    #Make 5 from SelectList to be a negative number.
+    #Format numbers with comma for decimal value
+
+    if float(NewList[1]) != 0:
+        DateDescriptionAmmoutList.append("-"+(NewList[1].replace(".",",")))
+
+    if float(NewList[2]) != 0:
+        DateDescriptionAmmoutList.append(NewList[2].replace(".",","))
+
+    #convert list to string in order to write to file
+    SaveString = ",".join(DateDescriptionAmmoutList)
+    ManagerStatement.write(SaveString + "\n")
+
+#close files
+BankStatement.close()
+ManagerStatement.close()
